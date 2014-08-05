611ed7c47da7a00ebdff255ef76233815a3cb951
Ruarc Sorensen <ruarcs@gmail.com>
2014-08-05 21:27:35 +0100

    Formatting changes for question 3

--- chapter1/Question3.java
+++ chapter1/Question3.java
@@ -3,21 +3,17 @@ package chapter1;
 import java.io.File;
 import java.io.IOException;
 
-public class Question3
-{
+public class Question3 {
 	private static final String TEST_DIR = "/Users/ruarc/Documents/workspace/Test/src/com/example/test";
-	
-	public static void main( String[] args ) throws IOException
-	{
+
+	public static void main(String[] args) throws IOException {
 		Question3 q = new Question3();
-		for( File file : q.filesWithExtension( new File( TEST_DIR ), ".java" ) )
-		{
-			System.out.println( file.getCanonicalPath() );
+		for (File file : q.filesWithExtension(new File(TEST_DIR), ".java")) {
+			System.out.println(file.getCanonicalPath());
 		}
 	}
-	
-	private File[] filesWithExtension( File file, String extension ) throws IOException
-	{
-		return file.listFiles( ( f, name ) -> name.endsWith( extension ) );
+
+	private File[] filesWithExtension(File file, String extension) throws IOException {
+		return file.listFiles((f, name) -> name.endsWith(extension));
 	}
 }
